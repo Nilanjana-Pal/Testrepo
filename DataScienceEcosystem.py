1d118d405be8462663651f38482a439425e92e77
Nilanjana-Pal <133552219+Nilanjana-Pal@users.noreply.github.com>
2023-08-31 17:34:01 +0800

    Add files via upload
    Assignment

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#Exercise 2: Create markdown cell with title of the notebook
+
+
+# # Data Science Tools and Ecosystem
+
+# In[2]:
+
+
+#Exercise 3 - Create a markdown cell for an introduction
+
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# **Objectives:**
+#     
+#     -List popular languages for Data Science
+#     
+#     -List popular tools used for Data Science
+#     
+#     -List commonly used libraries by Data Scientists
+
+# In[3]:
+
+
+#Exercise 4 - Create a markdown cell to list data science languages
+
+
+# Some of the popular languages that Data Scientists use are: 
+# 
+#     1. Python
+#     
+#     2. R
+#     
+#     3. SQL
+#     
+#     4. Julia
+
+# In[ ]:
+
+
+#Exercise 5 - Create a markdown cell to list data science libraries
+
+
+# Some of the commonly used libraries used by Data Scientists include:
+# 
+#     1.NumPy
+#     
+#     2.SciPy
+#     
+#     3.matplotlib
+#     
+#     4.Pandas
+
+# In[5]:
+
+
+#Exercise 6 - Create a markdown cell with a table of Data Science tools
+
+
+# Data Science Tools
+# ----------------------------
+# Jupyter Notebook/ JypyterLab
+# 
+# RStudio
+# 
+# Visual Studio Code
+
+# In[6]:
+
+
+#Exercise 7 - Create a markdown cell introducing arithmetic expression examples
+
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+
+# In[7]:
+
+
+#Exercise 8 - Create a code cell to multiply and add numbers
+
+
+# In[8]:
+
+
+# This a simple arithmetic expression to mutiply then add integers
+(3*4)+5
+
+
+# In[9]:
+
+
+# This will convert 200 minutes to hours by diving by 60
+minutes = 200
+hours = minutes / 60
+print(hours)
+
+
+# In[10]:
+
+
+#Exercise 11 - Create a markdown cell to indicate the Author's name
+    
+
+
+# ## Author
+# Nilanjana Pal
+
+# In[ ]:
+
+
+
+
